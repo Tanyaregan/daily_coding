3b0c16c768ffc84dbf8d55c902abd5e21ece4135
vagrant <vagrant@vagrant.vm>
2018-01-11 23:31:19 +0000

    first problem

--- daily_1_10.py
+++ daily_1_10.py
@@ -11,14 +11,14 @@
 
 # Hint: Try working backwards from the end state.
 
-def reverse_and_interleave(lst):
-    """Given a stack of elements, interleave the second half reversed
+def reverse_and_interleave_list(lst):
+    """Given a list of elements, interleave the second half reversed
         into the first half.
 
-    >>> reverse_and_interleave([1, 2, 3, 4, 5])
+    >>> reverse_and_interleave_list([1, 2, 3, 4, 5])
         [1, 5, 2, 4, 3]
 
-        reverse_and_interleave([1,2,3,4])
+        reverse_and_interleave_list([1, 2, 3, 4])
         [1, 4, 2, 3]
 
     """
@@ -26,10 +26,52 @@ def reverse_and_interleave(lst):
     i = 1
 
     while i < len(lst):
-        lst[i] = lst.pop()
+        lst.insert(i, lst.pop())
         i = i + 2
 
     return lst
+
+
+def reverse_and_interleave_stack(stack):
+    """Given a stack of elements, interleave the second half reversed
+        into the first half using a queue.
+
+    >>> reverse_and_interleave_list([1, 2, 3, 4, 5])
+        [1, 5, 2, 4, 3]
+
+        reverse_and_interleave_list([1,2,3,4])
+        [1, 4, 2, 3]
+
+    """
+
+# I'm going to pseudocode this, I'm not sure if it's possible
+# to do it this way with Python.
+# This is what I want to do using the [1, 2, 3, 4, 5] example:
+
+# stack = [1, 2, 3, 4, 5]
+# queue = []
+
+# Pop each element off the top of the stack and enqueue it:
+
+# queue = [1, 2, 3, 4, 5]
+# stack = []
+
+# Take an element from the head of the queue, add it to top of the stack,
+# and then take one from the tail and add it to the stack until you have:
+
+# stack = [3, 4, 2, 5, 1]
+# queue = []
+
+# Pop each element off the top of the stack and enqueue it:
+
+# queue = [3, 4, 2, 5, 1]
+# stack = []
+
+# take each tail out of the queue in turn and add it to the stack:
+
+# stack = [1, 5, 2, 4, 3]
+# queue = []
+
 
 
 #######################################################
